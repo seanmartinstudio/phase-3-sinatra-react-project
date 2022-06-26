501af87eb377789e1f014c337c2d949f9f5dcbce
Sean Martin <sean@seanmartinstudio.com>
2022-06-26 14:07:26 -0700

    DB Migrated

--- db/migrate/20220626194317_create_to_dos.rb
+++ db/migrate/20220626194317_create_to_dos.rb
@@ -1,4 +1,10 @@
 class CreateToDos < ActiveRecord::Migration[6.1]
   def change
+    create_table :to_dos do |t|
+      t.string :name
+      t.integer :category_id
+      t.boolean :complete
+      t.timestamps
+    end
   end
 end
